11773e5943f5a49bb882086957d268c3c6d70376
Hassan Nazar <hassannazar72edu@gmail.com>
2025-11-30 22:32:46 +0000

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.js
@@ -0,0 +1,16 @@
+// Last updated: 30/11/2025, 22:32:46
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+
+        }
+    }
+};
