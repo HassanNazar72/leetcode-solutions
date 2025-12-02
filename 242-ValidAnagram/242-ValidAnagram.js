6fdcc1533ea97e0651e24cc2e1b178d08d711550
Hassan Nazar <hassannazar72edu@gmail.com>
2025-12-02 14:06:23 +0000

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.js
@@ -0,0 +1,16 @@
+// Last updated: 02/12/2025, 14:06:23
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let s1 = s.split('').sort().join('')
+    let s2 = t.split('').sort().join('')
+    if(s1===s2){
+        return true
+    }
+    else{
+        return false
+    }
+};
