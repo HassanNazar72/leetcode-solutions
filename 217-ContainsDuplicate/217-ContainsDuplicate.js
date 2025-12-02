2ed4e1c295dd8c049adff88274ff64bf6e08cb5c
Hassan Nazar <hassannazar72edu@gmail.com>
2025-12-02 13:35:04 +0000

    File updated at 02/12/2025, 13:35:04

--- 217-ContainsDuplicate/217-ContainsDuplicate.js
+++ 217-ContainsDuplicate/217-ContainsDuplicate.js
@@ -1,10 +1,10 @@
-// Last updated: 02/12/2025, 13:31:48
+// Last updated: 02/12/2025, 13:35:04
 1/**
 2 * @param {number[]} nums
 3 * @return {boolean}
 4 */
 5var containsDuplicate = function(nums) {
-6    nums.sort((x, y) => x - y);
+6    nums.sort((x, y) => x - y);;
 7    for (let i = 0; i < nums.length; i++) {
 8        let count = 0
 9        if(nums[i]===nums[i+1]){
