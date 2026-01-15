690b4929a1cd3c4c37550c82dabedd309f6b66e5
Hassan Nazar <hassannazar72edu@gmail.com>
2026-01-15 19:08:46 +0000

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.js
@@ -0,0 +1,23 @@
+// Last updated: 15/01/2026, 19:08:46
+1/**
+2 * @param {string} s
+3 * @return {boolean}
+4 */
+5var isPalindrome = function(s) {
+6    let count = 0
+7    const result = s
+8  .toLowerCase()              // convert to lowercase
+9  .replace(/[^a-z0-9]/g, ''); // remove non-alphanumeric characters
+10    for(let i=0;i<result.length;i++){
+11        if(result[i]==result[result.length-1-i]){
+12            console.log(result[i],result[result.length-1-i])
+13            count += 1
+14        }
+15    }
+16    if(count==result.length){
+17        return true
+18    }
+19    else{
+20        return false
+21    }
+22};
