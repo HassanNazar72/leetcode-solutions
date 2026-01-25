93d6a22bcf460987b3239158b3005236e9c44b8a
Hassan Nazar <hassannazar72edu@gmail.com>
2026-01-25 18:26:21 +0000

    File updated at 25/01/2026, 18:26:21

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.js
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.js
@@ -1,4 +1,4 @@
-// Last updated: 25/01/2026, 17:55:24
+// Last updated: 25/01/2026, 18:26:21
 1/**
 2 * @param {number[]} nums
 3 * @return {number}
@@ -6,29 +6,23 @@
 5var longestConsecutive = function(nums) {
 6    if (nums.length === 0) return 0;
 7
-8    let sortednum = nums.sort((a, b) => a - b);
-9    let count = {};
-10    let key = 0;
-11
-12    count[key] = 1;
-13
-14    for (let i = 0; i < sortednum.length - 1; i++) {
-15
-16        // if consecutive
-17        if (sortednum[i] + 1 === sortednum[i + 1]) {
-18            count[key] += 1;
-19        }
-20        // if duplicate, ignore
-21        else if (sortednum[i] === sortednum[i + 1]) {
-22            continue;
-23        }
-24        
-25        else {
-26            key++;
-27            count[key] = 1;
-28        }
-29    }
-30
-31    return Math.max(...Object.values(count));
-32};
-33
+8    const set = new Set(nums)
+9    let max = 0
+10    
+11    for(let num of set){
+12        if(set.has(num -1)){
+13            continue
+14        }
+15        let currentnum = num
+16        let currentmax = 1;
+17
+18        while(set.has(currentnum+1)){
+19            currentnum ++;
+20            currentmax ++
+21        }
+22        max = Math.max(max, currentmax)
+23    }
+24    return max
+25    
+26};
+27
