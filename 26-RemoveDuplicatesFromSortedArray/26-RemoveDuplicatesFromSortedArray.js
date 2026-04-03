eb278cd9be6b44b650b13d51b814965e7a001369
Hassan Nazar <hassannazar72edu@gmail.com>
2026-04-03 22:58:13 +0100

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,20 @@
+// Last updated: 03/04/2026, 22:58:13
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let j = 0; // Pointer for the position of unique elements
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[j] !== nums[i]) {
+            j++;
+            nums[j] = nums[i]; // Move the unique element to the next position
+        }
+    }
+    return j + 1; // Return the length of the array with unique elements
+}
+
