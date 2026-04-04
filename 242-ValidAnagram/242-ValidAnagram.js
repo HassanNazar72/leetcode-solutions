86738ae1353c322cf8abe992a1f49523df85d0a4
Hassan Nazar <hassannazar72edu@gmail.com>
2026-04-04 17:55:41 +0100

    File updated at 04/04/2026, 17:55:41

--- 242-ValidAnagram/242-ValidAnagram.js
+++ 242-ValidAnagram/242-ValidAnagram.js
@@ -1,16 +1,24 @@
-// Last updated: 02/12/2025, 14:06:23
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
-var isAnagram = function(s, t) {
-    let s1 = s.split('').sort().join('')
-    let s2 = t.split('').sort().join('')
-    if(s1===s2){
-        return true
-    }
-    else{
-        return false
-    }
-};
+// Last updated: 04/04/2026, 17:55:41
+1/**
+2 * @param {string} s
+3 * @param {string} t
+4 * @return {boolean}
+5 */
+6var isAnagram = function(s, t) {
+7    if (s.length !== t.length){
+8        return false
+9    }
+10    const count = new Map()
+11
+12    for(const char of s){
+13        count.set(char, (count.get(char) || 0) + 1);
+14    }
+15
+16    for(const char of t){
+17        if(!count.has(char)){return false}
+18
+19        count.set(char,count.get(char)-1)
+20    if (count.get(char) < 0) return false;
+21    }
+22    return true
+23};
