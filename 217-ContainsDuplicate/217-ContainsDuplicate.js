7d2e98fde1f6e4a12149437ea277b6a7077e5101
Hassan Nazar <hassannazar72edu@gmail.com>
2026-04-04 16:29:48 +0100

    File updated at 04/04/2026, 16:29:48

--- 217-ContainsDuplicate/217-ContainsDuplicate.js
+++ 217-ContainsDuplicate/217-ContainsDuplicate.js
@@ -1,16 +1,16 @@
-// Last updated: 02/12/2025, 14:05:53
+// Last updated: 04/04/2026, 16:29:48
 1/**
-2 * @param {string} s
-3 * @param {string} t
-4 * @return {boolean}
-5 */
-6var isAnagram = function(s, t) {
-7    let s1 = s.split('').sort().join('')
-8    let s2 = t.split('').sort().join('')
-9    if(s1===s2){
-10        return true
-11    }
-12    else{
-13        return false
-14    }
-15};
+2 * @param {number[]} nums
+3 * @return {boolean}
+4 */
+5var containsDuplicate = function(nums) {
+6    let seen = new Set();
+7    for(let i=0;i<nums.length;i++){
+8        if(seen.has(nums[i])){
+9            return true;
+10        }
+11        seen.add(nums[i])
+12    }
+13    return false;
+14};
+15
