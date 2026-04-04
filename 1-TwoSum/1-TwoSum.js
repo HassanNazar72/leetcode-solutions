a9823a8dea8b1e069570fc1e2fb4237344d93596
Hassan Nazar <hassannazar72edu@gmail.com>
2026-04-04 16:09:45 +0100

    File updated at 04/04/2026, 16:09:45

--- 1-TwoSum/1-TwoSum.js
+++ 1-TwoSum/1-TwoSum.js
@@ -1,19 +1,23 @@
-// Last updated: 11/12/2025, 14:41:05
+// Last updated: 04/04/2026, 16:09:45
 1/**
 2 * @param {number[]} nums
 3 * @param {number} target
 4 * @return {number[]}
 5 */
 6var twoSum = function(nums, target) {
-7    for (let i = 0; i < nums.length; i++) {
-8        for (let j = i + 1; j < nums.length; j++) {
-9            if (nums[i] + nums[j] === target) {
-10                return [i, j];
-11            }
-12        }
-13    }
-14    return null;
-15};
+7    const seen = new Map();
+8
+9    for(let i =0;i<nums.length;i++){
+10        const complement = target - nums[i]
+11
+12        if(seen.has(complement)){
+13            return [seen.get(complement),i]
+14        }
+15        seen.set(nums[i],i)
 16
-17
-18
+17    }
+18    return null
+19};
+20
+21
+22
