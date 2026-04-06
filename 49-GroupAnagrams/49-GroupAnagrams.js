34ed6a046ef04d0c63ac4ea3f74269b7b3f1f470
Hassan Nazar <hassannazar72edu@gmail.com>
2026-04-06 16:44:00 +0100

    File updated at 06/04/2026, 16:44:00

--- 49-GroupAnagrams/49-GroupAnagrams.js
+++ 49-GroupAnagrams/49-GroupAnagrams.js
@@ -1,27 +1,21 @@
-// Last updated: 03/12/2025, 20:43:34
+// Last updated: 06/04/2026, 16:44:00
 1/**
 2 * @param {string[]} strs
 3 * @return {string[][]}
 4 */
 5var groupAnagrams = function(strs) {
 6    let map = {};
-7
-8    for (let i = 0; i < strs.length; i++) {
-9        let chars = strs[i].split("");
-10        let sorted = chars.sort().join("");
-11
-12        if (!map[sorted]) {
-13            map[sorted] = [];
-14        }
+7  for(let i=0;i<strs.length;i++){
+8    let sorted = strs[i].split("").sort().join("")
+9
+10    if(!map[sorted]){
+11        map[sorted] = []
+12    }
+13    map[sorted].push(strs[i])
+14  }
 15
-16        map[sorted].push(strs[i]);
-17    }
+16    return Object.values(map);
+17};
 18
-19    let output = Object.values(map);
+19
 20
-21    console.log(JSON.stringify(output));
-22    return output;
-23};
-24
-25
-26
