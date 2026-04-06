af2032ca597143faf29602c1b7d2fe692cac51cb
Hassan Nazar <hassannazar72edu@gmail.com>
2026-04-06 20:39:07 +0100

    File updated at 06/04/2026, 20:39:07

--- 347-TopKFrequentElements/347-TopKFrequentElements.js
+++ 347-TopKFrequentElements/347-TopKFrequentElements.js
@@ -1,30 +1,33 @@
-// Last updated: 10/01/2026, 18:59:15
+// Last updated: 06/04/2026, 20:39:07
 1/**
 2 * @param {number[]} nums
 3 * @param {number} k
 4 * @return {number[]}
 5 */
 6var topKFrequent = function(nums, k) {
-7    let map = {};
-8
-9    //Build map with arrays
-10    for (let i = 0; i < nums.length; i++) {
-11        if (!map[nums[i]]) {
-12            map[nums[i]] = 1;
-13        } else {
-14            map[nums[i]] += 1;
-15        }
-16    }
-17    //sort
-18    let freqArray = Object.entries(map).sort((a, b) => b[1] - a[1]);
-19
-20    //Pick top k elements
-21    let result = [];
-22
-23    for (let i = 0; i < k; i++) {
-24        result.push(Number(freqArray[i][0]));
-25    }
-26
-27    return result;
-28};
-29
+7    // Step 1: count frequency of each number
+8    const map = {};
+9    for (const num of nums) {
+10        map[num] = (map[num] || 0) + 1;
+11    }
+12
+13    // Step 2: create empty buckets where index = frequency
+14    const buckets = [];
+15    for (let i = 0; i <= nums.length; i++) {
+16        buckets[i] = [];
+17    }
+18    // Step 3: place each number into bucket matching its frequency
+19    for(const num in map){
+20        const freq = map[num]
+21        buckets[freq].push(Number(num))
+22    }
+23    // Step 4: read from right to left, grab k elements
+24    const result = [];
+25    for (let i = buckets.length - 1; i >= 0; i--) {
+26        for (const num of buckets[i]) {
+27            result.push(num);
+28            if (result.length === k) return result;
+29        }
+30    }
+31};
+32
